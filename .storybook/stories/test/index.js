b175c1ac08f132f8ce606527e9a630340324fdb1
Siriwat Kunporn <siriwatkunaporn@gmail.com>
2016-11-08 22:33:28 +0700

    add bulma and font awesome to storybook

--- .storybook/stories/test/index.js
+++ .storybook/stories/test/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { storiesOf, action, linkTo } from '@kadira/storybook';
 import {withKnobs, text, boolean, number, object, select} from '@kadira/storybook-addon-knobs';
+import '../../../static/bulma-0.2.3/css/bulma.css';
+import '../../../static/font-awesome-4.6.3/css/font-awesome.css';
 
 const stories = storiesOf('Test',module);
 stories.addDecorator(withKnobs);
@@ -8,4 +10,15 @@ stories.addDecorator(withKnobs);
 stories.add('Button',() => {
 	const label = text('label','label');
 	return <button>{label}</button>
+});
+
+stories.add('Bulma fontAwesome',() => {
+	return(
+		<a className="button">
+			<span className="icon">
+				<i className="fa fa-github"/>
+			</span>
+			<span>GitHub</span>
+		</a>
+	)
 });
